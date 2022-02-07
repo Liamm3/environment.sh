e827ce201f899b7546446c42a9d55d98af5fa5f6
Liamm3 <liam.wohlfart0@gmail.com>
2022-02-07 21:42:29 +0100

    Initial commit

---
+++ environment-install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Liamm3/my-slstatus.git
+git clone https://github.com/Liamm3/my-dmenu-with-some-patches.git
+git clone https://github.com/Liamm3/my-dwm-fork.git
+git clone https://github.com/Liamm3/arch-install-dependencies.git
+git clone https://github.com/Liamm3/config-files.git
